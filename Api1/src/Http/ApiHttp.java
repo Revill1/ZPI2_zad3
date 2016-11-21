936399d6f0d6272acf6f5fb05d517de86f2dc4cf
Pelka <wojciech.pelka@accenture.com>
2016-11-21 20:32:52 +0100

    ApiHttp class added

--- Api1/src/Http/ApiHttp.java
+++ Api1/src/Http/ApiHttp.java
@@ -1,5 +1,15 @@
 package Http;
 
-public class ApiHttp {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.MalformedInputException;
 
+public class ApiHttp 
+{
+	
 }
