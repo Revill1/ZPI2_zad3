31365e0ce882567b391a7fe26b693c54cf6e2f7e
Pelka <wojciech.pelka@accenture.com>
2016-11-21 20:46:14 +0100

    unzip method added

--- Api1/src/ZipArchive/ApiZipArchive.java
+++ Api1/src/ZipArchive/ApiZipArchive.java
@@ -1,9 +1,11 @@
 package ZipArchive;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ApiZipArchive 
@@ -35,5 +37,52 @@ public class ApiZipArchive
     	}catch(IOException ex){
     	   ex.printStackTrace();
     	}
+	}
+	
+	public void Unzip(String zipFile, String outputFolder)
+	{
+		byte[] buffer = new byte[1024];
+
+	     try{
+
+	    	File folder = new File(outputFolder);
+	    	if(!folder.exists()){
+	    		folder.mkdir();
+	    	}
+
+	    	ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+	    	
+	    	ZipEntry ze = zis.getNextEntry();
+
+	    	while(ze!=null){
+
+	    	   String fileName = ze.getName();
+	           File newFile = new File(outputFolder + File.separator + fileName);
+
+	           System.out.println("file unzip : "+ newFile.getAbsoluteFile());
+
+	            //create all non exists folders
+	            //else you will hit FileNotFoundException for compressed folder
+	            new File(newFile.getParent()).mkdirs();
+
+	            FileOutputStream fos = new FileOutputStream(newFile);
+
+	            int len;
+	            while ((len = zis.read(buffer)) > 0) {
+	       		fos.write(buffer, 0, len);
+	            }
+
+	            fos.close();
+	            ze = zis.getNextEntry();
+	    	}
+
+	        zis.closeEntry();
+	    	zis.close();
+
+	    	System.out.println("Done");
+
+	    }catch(IOException ex){
+	       ex.printStackTrace();
+	    }
 	}
 }
