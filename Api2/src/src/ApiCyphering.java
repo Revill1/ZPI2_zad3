31f813116a5961f801ec3a03516f412491105c9a
Revill1 <adamredlisnki@gmail.com>
2016-11-21 21:30:14 +0100

    dodane metody szyfrująca i deszyfrująca do zaimplementowania

--- Api2/src/src/ApiCyphering.java
+++ Api2/src/src/ApiCyphering.java
@@ -1,5 +1,26 @@
 package src;
 
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 public class ApiCyphering {
+	
+	
+	public static byte[] encrypt(String strToEncrypt, byte[] key)
+    {
+		
+        return null;
+    }
+
+    public static String decrypt(byte[] byteToDecrypt, byte[] key)
+    {
+      
+        return null;
+    }
+
 
 }
