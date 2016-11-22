cf42025b0a636160dd46ba8849d869a07d87d87f
Pelka <wojciech.pelka@accenture.com>
2016-11-22 10:42:54 +0100

    tests classes added

---
+++ Api1/tests/FileLibraryTests/FileLibraryTests.java
@@ -0,0 +1,14 @@
+package FileLibraryTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileLibraryTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Api1/tests/HttpApiTests/HttpApiTests.java
@@ -0,0 +1,14 @@
+package HttpApiTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HttpApiTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
