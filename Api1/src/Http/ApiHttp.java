8c038d84107b2adba7850b4ce656ce30f228589d
WojtekP12 <wojciech.pelka@onet.pl>
2016-11-28 22:11:26 +0100

    Http library refactored, DownloadFile test added

--- Api1/src/Http/ApiHttp.java
+++ Api1/src/Http/ApiHttp.java
@@ -3,8 +3,10 @@ package Http;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.MalformedInputException;
@@ -20,19 +22,19 @@ public class ApiHttp
 	
 	public File DowloadFile(String destinationPath) throws IOException
 	{
-		int i;
-		
-		URL url = new URL(address); 
-		URLConnection con = url.openConnection();
-		File file = new File(destinationPath);
-		BufferedInputStream bis = new BufferedInputStream(con.getInputStream());
-		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file.getName()));
-		
-		while((i=bis.read())!=-1)
-		{
-			bos.write(i);
-		}
-		
-		return null;
+		URL url = new URL(address);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+        FileOutputStream fis = new FileOutputStream(destinationPath);
+        byte[] buffer = new byte[1024];
+        int count=0;
+        while((count = bis.read(buffer,0,1024)) != -1)
+        {
+            fis.write(buffer, 0, count);
+        }
+        
+        fis.close();
+        bis.close();
+        
+        return null;
 	}
 }
--- Api1/tests/HttpApiTests/HttpApiTests.java
+++ Api1/tests/HttpApiTests/HttpApiTests.java
@@ -2,13 +2,24 @@ package HttpApiTests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
+
+import Library.FileLibrary;
 
 public class HttpApiTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void DownloadFromUriTest() throws IOException 
+	{
+		FileLibrary lib = new FileLibrary();
+		lib.DownloadFileFromRoute("http://mmajchr.kis.p.lodz.pl/zpi2/zadania/3a.txt", "C:/Users/Wojciech/Desktop/3a.txt");
+		
+		File file = new File("C:/Users/Wojciech/Desktop/3a.txt");
+		
+		assertTrue(file.exists());
 	}
 
 }
