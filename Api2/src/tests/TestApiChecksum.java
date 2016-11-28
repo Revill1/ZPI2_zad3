773bd8e0497a3c4613aa1dbd1f3954499fedc767
Revill1 <adamredlisnki@gmail.com>
2016-11-28 23:23:12 +0100

    added test for test checksum with mda alghoritm

--- Api2/src/tests/TestApiChecksum.java
+++ Api2/src/tests/TestApiChecksum.java
@@ -2,11 +2,15 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,6 +25,7 @@ public class TestApiChecksum {
 	private String algorithmSHA;
 	private String compareSum;
 	private Object compareSumSHA;
+	private String file;
 
 	@Before
 	public void init()
@@ -30,6 +35,20 @@ public class TestApiChecksum {
 		algorithmSHA = "SHA";
 		compareSum = "ff0b87f35443325263d5032b303eea18";
 		compareSumSHA = "ad68a0cd4a653a34c23eb9818401879a89bf8fb0";
+		file = "file";
+		
+		File newFile = new File(file);
+		try {
+			newFile.createNewFile();
+			FileOutputStream fos = new FileOutputStream(newFile);
+			fos.write(input.getBytes());
+			fos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
 	@Test
@@ -55,6 +74,16 @@ public class TestApiChecksum {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	}
+	
+	@Test
+	public void testChecksumWithMDAAlghoritmTest()
+	{
+		try {
+			Assert.assertTrue(ApiChecksum.testChecksum(file, compareSum, algorithm));
+		} catch (NoSuchAlgorithmException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
