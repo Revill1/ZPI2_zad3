c00b144791073016f815769be17ead2223f201d5
Pelka <wojciech.pelka@accenture.com>
2016-11-29 10:51:06 +0100

    Archive tests added

--- Api1/tests/FileLibraryTests/FileLibraryTests.java
+++ Api1/tests/FileLibraryTests/FileLibraryTests.java
@@ -2,13 +2,47 @@ package FileLibraryTests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
+
+import Library.FileLibrary;
 
 public class FileLibraryTests {
 
+	FileLibrary lib = new FileLibrary();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void DownloadFromUrlTest() throws IOException 
+	{
+		lib.DownloadFileFromRoute("http://mmajchr.kis.p.lodz.pl/zpi2/zadania/3a.txt", "C:/Users/wojciech.pelka/Desktop/3a.txt");
+		
+		File file = new File("C:/Users/wojciech.pelka/Desktop/3a.txt");
+		
+		assertTrue(file.exists());
+	}
+	
+	@Test
+	public void AddFileToArchiveTest()
+	{
+		lib.AddFileToArchive("C:/Users/wojciech.pelka/Desktop/TEST.txt", "C:/Users/wojciech.pelka/Desktop/TestZIP.zip");
+		
+		File archivedFile = new File("C:/Users/wojciech.pelka/Desktop/TestZIP.zip");
+		
+		assertTrue(archivedFile.exists());
+	}
+	
+	@Test
+	public void ExtractFileFromArchiveTest()
+	{
+		lib.ExtractFileFromArchive("C:/Users/wojciech.pelka/Desktop/TestZIP.zip", "C:/Users/wojciech.pelka/Desktop/TestZIP");
+		
+		File extractedFile = new File("C:/Users/wojciech.pelka/Desktop/TestZIP");
+		
+		assertTrue(extractedFile.isDirectory());
+		assertTrue(extractedFile.list().length>0);
+		
 	}
 
 }
